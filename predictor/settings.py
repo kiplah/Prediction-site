8e11fb3f710b68dbeea6b1e59997e22b3902aae5
kiplah <kiplahvictor27@gmail.com>
2025-05-08 17:22:51 +0300

    updated

--- predictor/settings.py
+++ predictor/settings.py
@@ -140,3 +140,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
--- tips/views.py
+++ tips/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from .models import Match, Prediction
 from ..users.serializers import MatchSerializer, PredictionSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class MatchViewSet(viewsets.ModelViewSet):
     queryset = Match.objects.all().order_by('-match_date')
@@ -17,3 +18,11 @@ class PredictionViewSet(viewsets.ModelViewSet):
         if user.is_authenticated and getattr(user, 'is_premium', False):
             return self.queryset
         return self.queryset.filter(is_premium=False)
+class PredictionViewSet(viewsets.ModelViewSet):
+    ...
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'match__match_date': ['gte', 'lte'],
+        'match__league': ['exact', 'icontains'],
+        'is_premium': ['exact']
+    }
