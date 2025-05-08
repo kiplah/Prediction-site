cc3a7ba7fdabad172f727a0f2942a55811cdf60b
kiplah <kiplahvictor27@gmail.com>
2025-05-08 16:57:47 +0300

    updated views

--- tips/views.py
+++ tips/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Match, Prediction
+from .serializers import MatchSerializer, PredictionSerializer
 
-# Create your views here.
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = Match.objects.all().order_by('-match_date')
+    serializer_class = MatchSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class PredictionViewSet(viewsets.ModelViewSet):
+    queryset = Prediction.objects.all().order_by('-created_at')
+    serializer_class = PredictionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated and getattr(user, 'is_premium', False):
+            return self.queryset
+        return self.queryset.filter(is_premium=False)
