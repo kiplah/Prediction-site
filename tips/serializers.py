49c800e6960f3fc4c48111870b15ede1f33c4e75
kiplah <kiplahvictor27@gmail.com>
2025-05-08 17:05:00 +0300

    updated

--- tips/serializers.py
+++ tips/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Match, Prediction
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +15,8 @@ class PredictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prediction
         fields = '__all__'
+class PremiumStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'is_premium']
+        read_only_fields = ['id', 'email']
